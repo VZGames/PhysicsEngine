3ea9783170ccac67e938feb91be6f69f9e4e2a21
Tran Phuoc Tin <tptin.76@gmail.com>
2024-05-11 01:51:54 +0700

    update

--- Math/Shape2D.h
+++ Math/Shape2D.h
@@ -2,6 +2,7 @@
 #define SHAPE2D_H
 
 #include "Vector2D.h"
+#define PI 3.14f
 
 typedef enum e_shape_type
 {
@@ -37,4 +38,33 @@ typedef struct capsule
     float radius;
 } Capsule;
 
+float area(ShapeType type, void* define)
+{
+    switch (type) {
+    case CIRCLE:
+    {
+        Circle* circle = (Circle*)define;
+        return (PI * (circle->radius * circle->radius));
+    }
+    case ELLIPSE:
+    {
+        Ellipse* ellipse = (Ellipse*)define;
+        return (PI * (ellipse->a * ellipse->b));
+    }
+    case CAPSULE:
+    {
+        Capsule* capsule = (Capsule*)define;
+        float circle_area = (PI * (capsule->radius * capsule->radius));
+        float rectangle_area = (2 * capsule->radius) * (2 * capsule->radius) * (capsule->height - 1);
+        return circle_area + rectangle_area;
+    }
+    case POLYGON:
+    {
+        break;
+    }
+    default:
+        return 0.0f;
+    }
+    return 0.0f;
+}
 #endif // SHAPE2D_H
