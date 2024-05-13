443eaad94a8afbafbe8f9806f3984acf74769762
Tran Phuoc Tin <tptin.76@gmail.com>
2024-05-13 22:55:29 +0700

    update

--- DataStructs/QuadTree.h
+++ DataStructs/QuadTree.h
@@ -10,14 +10,15 @@ typedef enum quad_tree_node: int
     WestNorth,
     EastNorth,
     EastSouth,
-    WestSouth
+    WestSouth,
+    NodeLimit
 } QuadtreeNode;
 
 struct QuadTree
 {
     Rect2D rect; // boundary
     void** objects; // list of objects
-    QuadTree* nodes[4]; // list of nodes
+    QuadTree* nodes[QuadtreeNode::NodeLimit]; // list of nodes
 };
 
 int hash(QuadTree* node, float x, float y); // get cell index
@@ -26,17 +27,43 @@ QuadTree* CreateQuadTreeNode(float window_width, float window_height)
     QuadTree* root = (QuadTree*)malloc(sizeof(QuadTree));
     root->rect.A = (Vec2){0, 0};
     root->rect.C = (Vec2){window_width, window_height};
-    root->nodes[WestNorth] = NULL;
-    root->nodes[EastNorth] = NULL;
-    root->nodes[EastSouth] = NULL;
-    root->nodes[WestSouth] = NULL;
+    Vec2 size = subtract(root->rect.C, root->rect.A);
+
+    for (int i = QuadtreeNode::WestNorth; i < QuadtreeNode::NodeLimit; i++)
+    {
+        root->nodes[i] = (QuadTree*)malloc(sizeof(QuadTree));
+    }
+
+    root->nodes[WestNorth]->rect.A.x    =  root->rect.A.x;
+    root->nodes[WestNorth]->rect.A.y    =  root->rect.A.y;
+    root->nodes[WestNorth]->rect.C.x    =  root->rect.A.x + size.x/2;
+    root->nodes[WestNorth]->rect.C.y    =  root->rect.A.y + size.y/2;
+
+    root->nodes[EastNorth]->rect.A.x    =  root->rect.A.x + size.x/2;
+    root->nodes[EastNorth]->rect.A.y    =  root->rect.A.y;
+    root->nodes[EastNorth]->rect.C.x    =  root->rect.A.x + size.x;
+    root->nodes[EastNorth]->rect.C.y    =  root->rect.A.y + size.y/2;
+
+    root->nodes[EastSouth]->rect.A.x    =  root->rect.A.x;
+    root->nodes[EastSouth]->rect.A.y    =  root->rect.A.y + size.y/2;
+    root->nodes[EastSouth]->rect.C.x    =  root->rect.A.x + size.x/2;
+    root->nodes[EastSouth]->rect.C.y    =  root->rect.A.y + size.y;
+
+    root->nodes[WestSouth]->rect.A.x    =  root->rect.A.x + size.x/2;
+    root->nodes[WestSouth]->rect.A.y    =  root->rect.A.y + size.y/2;
+    root->nodes[WestSouth]->rect.C.x    =  root->rect.C.x;
+    root->nodes[WestSouth]->rect.C.y    =  root->rect.C.y;
     return root;
 }
 
 void Insert(QuadTree* node, void* obj)
 {
+    if (obj == NULL) return;
+    if (node == NULL) return;
     B2D* body = (B2D*)obj;
     int index = hash(node, body->position.x, body->position.y);
+    Insert(node->nodes[(QuadtreeNode)index], NULL); // have not anchor yet
+
 }
 
 int hash(QuadTree* node, float x, float y) // get cell index
